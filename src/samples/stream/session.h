4c17b5b632e46df1e44316587e03c5d4f255df78
sniperHW <sniperHW@163.com>
2015-06-09 01:36:06 +0800

    none

--- src/samples/stream/session.h
+++ src/samples/stream/session.h
@@ -8,26 +8,26 @@ typedef struct {
 struct session{
 	my_ioreq send_overlap;
 	my_ioreq recv_overlap;
-	struct iovec wbuf[1];
-	char   	  buf[65535];
+	struct iovec wbuf;
+	char         buf[1024];
 	stream_socket_   *s;
 };
 
 int32_t session_recv(struct session *s,int flag)
 {
-	s->wbuf[0].iov_base = s->buf;
-	s->wbuf[0].iov_len = 65535;
+	s->wbuf.iov_base = s->buf;
+	s->wbuf.iov_len = 1024;
 	s->recv_overlap.base.iovec_count = 1;
-	s->recv_overlap.base.iovec = s->wbuf;
+	s->recv_overlap.base.iovec = &s->wbuf;
 	return stream_socket_recv(s->s,(iorequest*)&s->recv_overlap,flag);
 }
 
 int32_t session_send(struct session *s,int32_t size,int flag)
 {
-	s->wbuf[0].iov_base = s->buf;
-   	s->wbuf[0].iov_len = size;
+	s->wbuf.iov_base = s->buf;
+   	s->wbuf.iov_len = size;
 	s->send_overlap.base.iovec_count = 1;
-	s->send_overlap.base.iovec = s->wbuf;
+	s->send_overlap.base.iovec = &s->wbuf;
 	return stream_socket_send(s->s,(iorequest*)&s->send_overlap,flag);  	 
 }
 
@@ -40,36 +40,16 @@ void transfer_finish(handle *h,void *_,int32_t bytestransfer,int32_t err){
     my_ioreq *req = ((my_ioreq*)_);
     struct session *s = req->s; 
     if(!req || bytestransfer <= 0)
-    {
-//socket_err:    	
+    {  	
         close_socket((socket_*)h);
         free(s);
         --client_count;           
         return;
     }
 
-/*    do{
-    	if(req == &s->send_overlap){
-    		bytestransfer = session_recv(s,IO_NOW);
-    		if(bytestransfer == 0 ||
-    		  (bytestransfer < 0 && bytestransfer != -EAGAIN))	
-    			goto socket_err;
-    		req = &s->recv_overlap;
-    	}else{
-			totalbytes += bytestransfer;
-			packet_recv++;    		
-    		bytestransfer = session_send(s,bytestransfer,IO_NOW);
-    		if(bytestransfer == 0 ||
-    		  (bytestransfer < 0 && bytestransfer != -EAGAIN))	
-    			goto socket_err;
-    		req = &s->send_overlap;
-    	}
-    }while(bytestransfer > 0);
-*/
-    //if(req == &s->send_overlap)
-		
+    if(req == &s->send_overlap)
+		session_recv(s,IO_POST);
     if(req == &s->recv_overlap){
-    	session_recv(s,IO_POST);
 		session_send(s,bytestransfer,IO_POST);
 		totalbytes += bytestransfer;
 		packet_recv++;
