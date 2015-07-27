3cd0641927679975c87bab6b34a6817d21eade27
sniperHW <sniperHW@163.com>
2015-07-27 17:52:46 +0800

    none

--- distri/test/httpclient.lua
+++ distri/test/httpclient.lua
@@ -3,19 +3,22 @@ local Distri = require("distri.distri")
 local Chuck  = require("chuck")
 local Socket = require("distri.socket")
 
-local client    = Http.Client("www.baidu.com")
+local client    = Http.Client("s1.jctt.mmgame.mobi")
 if client then
-	local request = Http.Request("/")
-	client:Get(request,function (response,errno)
-		if response then
-			for k,v in pairs(response:Headers()) do
-				print(v[1] .. " : " .. v[2])
+	for i = 1,100 do
+		local request = Http.Request("/index.php/ajaxgateway/index/stadium/getUserServerPvp")
+		client:Post(request,function (response,errno)
+			if response then
+				for k,v in pairs(response:Headers()) do
+					print(v[1] .. " : " .. v[2])
+				end
+				--print(response:Content())
+				print(#response:Content(),i)
+			else
+				print("request error:" .. errno)
 			end
-			print(response:Content())
-		else
-			print("request error:" .. errno)
-		end
-	end)
+		end)
+	end
 	Distri.Signal(Chuck.signal.SIGINT,Distri.Stop)
 	Distri.Run()	
 end
