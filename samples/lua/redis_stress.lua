d877c99d26d6dd25cfc07b3bded7ed25e19e18aa
sniperHW <huangweilook@21n.com>
2015-08-03 19:13:38 +0800

    none

--- samples/lua/redis_stress.lua
+++ samples/lua/redis_stress.lua
@@ -12,6 +12,9 @@ local signaler = signal.signaler(signal.SIGINT)
 
 if client then
 	local count = 0
+	local request = 0
+	local response = 0
+	local max = 1000000
 
 	local function gen_callback(cb,ud)
 		return function (conn,reply)
@@ -21,30 +24,78 @@ if client then
 
 	local function pop_cb(conn,reply,id)
 		count = count + 1
-		local cmd = string.format("LPUSH list %d",reply)
-		client:Execute(cmd)
-		cmd = string.format("lpop list")
-		client:Execute(cmd,gen_callback(pop_cb))				
-		--local cmd = string.format("hmget chaid:%d chainfo skills",id)
-	    --client:Execute(cmd,gen_callback(query_cb,id))		
+		response = response + 1
+		if response < max then
+			request = request + 2
+			local cmd = string.format("LPUSH list %d",reply)
+			client:Execute(cmd,function ()
+				count = count + 1
+				response = response + 1			
+			end)
+			cmd = string.format("lpop list")
+			client:Execute(cmd,gen_callback(pop_cb))
+		end					
 	end
 
+	local function incr_cb(conn,reply,id)
+		count = count + 1
+		response = response + 1
+		if response < max then
+			request = request + 1				
+			local cmd = string.format("INCR counter")
+		    client:Execute(cmd,gen_callback(incr_cb))
+		end
+	end
+
+	local function query_cb(conn,reply,id)
+		count = count + 1
+		response = response + 1
+		if response < max then
+			request = request + 1					
+			local cmd = string.format("hmget chaid:%d chainfo skills",id)
+		    client:Execute(cmd,gen_callback(query_cb,id))
+	  	end	
+	end
+	
+	client:Execute("set counter 1")
 
 	for i = 1,1000 do
-		local cmd = string.format("LPUSH list %d",i)
+		local cmd = string.format("hmset chaid:%d chainfo %s skills %s",i,
+								  "fasdfasfasdfasdfasdfasfasdfasfasdfdsaf",
+								  "fasfasdfasdfasfasdfasdfasdfcvavasdfasdf")
 		client:Execute(cmd)
+	end
+
+	for i = 1,1000 do
+		request = request + 1
+		local cmd = string.format("LPUSH list %d",i)
+		client:Execute(cmd,function ()
+			count = count + 1
+			response = response + 1			
+		end)
 	end
 
 	for i = 1,2 do
+		request = request + 1
 		local cmd = string.format("lpop list")
 		client:Execute(cmd,gen_callback(pop_cb))
 	end
+
+	for i = 1,2 do
+		request = request + 1
+		local cmd = string.format("hmget chaid:%d chainfo skills",i)
+	    client:Execute(cmd,gen_callback(query_cb,i))	
+	end
+
+	request = request + 1
+	client:Execute("incr counter",incr_cb)
+
 	local last = chuck.systick()
 	local t = chuck.RegTimer(engine,1000,
 				   function() 
 				   		collectgarbage("collect") 
 				   		local now = chuck.systick()
-				   		print("hmget:" .. count*1000/(now-last) .. "/s")
+				   		print("hmget:" .. count*1000/(now-last) .. "/s" .. " request:" .. request .. " response:" .. response)
 				   		last = now
 				   		count = 0 
 				   end)
