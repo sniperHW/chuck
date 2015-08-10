fcd6e5f5b80643b10d9d1dbb742589b41827eefa
sniperHW <huangweilook@21n.com>
2015-08-10 12:05:51 +0800

    重构代码

--- Refactor/src/socket/chk_stream_socket.c
+++ Refactor/src/socket/chk_stream_socket.c
@@ -182,6 +182,227 @@ static inline void update_next_recv_pos(chk_stream_socket *s,int32_t bytes) {
 	};
 }
 
+static void release_socket(chk_stream_socket *s) {
+	chk_bytebuffer  *b;
+	chk_events_remove(cast(chk_handle*,s));	
+	if(s->next_recv_buf) chk_bytechunk_release(s->next_recv_buf);
+	if(s->option.decoder) chk_decoder_del(s->option.decoder);
+	if(s->timer) chk_timer_unregister(s->timer);
+	while((b = cast(chk_bytebuffer*,chk_list_pop(&s->send_list))))
+		chk_bytebuffer_del(b);
+	close(s->fd);
+	free(s);
+}
+
+int32_t last_timer_cb(uint64_t tick,void*ud) {
+	//发送已经超时,直接释放
+	chk_stream_socket *s = cast(chk_stream_socket*,ud);
+	s->timer   = NULL;
+	//timer事件在所有套接口事件之后才处理,所以这里释放是安全的
+	release_socket(s);
+	return -1;
+}
+
+void chk_stream_socket_close(chk_stream_socket *s) {
+	if((s->status & SOCKET_CLOSE) || (s->status & SOCKET_HCLOSE)) 
+		return;
+	
+	if(!(s->status & SOCKET_PEERCLOSE) && !chk_list_empty(&s->send_list) && s->loop) {
+		s->status |= SOCKET_HCLOSE;
+		chk_disable_read(cast(chk_handle*,s));
+		shutdown(s->fd,SHUT_RD);
+		//数据还没发送完,设置5秒超时等待数据发送
+		s->timer = chk_loop_addtimer(s->loop,5000,last_timer_cb,s);
+	}else {
+		s->status |= SOCKET_CLOSE;		
+		if(!(s->status & SOCKET_INLOOP)){
+			release_socket(s);
+		}
+	}
+}
+
+void chk_stream_socket_setUd(chk_stream_socket *s,void*ud) {
+	s->ud = ud;
+}
+
+void *chk_stream_socket_getUd(chk_stream_socket *s) {
+	return s->ud;
+}
+
+
+#ifdef _LINUX_ET
+
+//使用linux edge trigger
+
+int32_t chk_stream_socket_send(chk_stream_socket *s,chk_bytebuffer *b) {
+	int32_t ret = 0;
+	int32_t status = s->status;
+	do {
+		if((status & SOCKET_CLOSE) || (status & SOCKET_HCLOSE) || 
+		   (status & SOCKET_PEERCLOSE)) {
+			chk_bytebuffer_del(b);	
+			ret = -1;
+			break;
+		}
+		if(b) chk_list_pushback(&s->send_list,cast(chk_list_entry*,b));
+		ret = process_write(s);
+	}while(0);
+	return ret;	
+}
+
+int32_t chk_stream_socket_put(chk_stream_socket *s,chk_bytebuffer *b) {
+	int32_t ret = 0;
+	int32_t status = s->status;
+	do {
+		if((status & SOCKET_CLOSE) || (status & SOCKET_HCLOSE) || 
+		   (status & SOCKET_PEERCLOSE)) {
+			chk_bytebuffer_del(b);	
+			ret = -1;
+			break;
+		}
+		if(b) chk_list_pushback(&s->send_list,cast(chk_list_entry*,b));
+	}while(0);
+	return ret;
+}
+
+int32_t chk_stream_socket_flush(chk_stream_socket *s) {
+	return chk_stream_socket_send(s,NULL);
+}
+
+int32_t chk_stream_socket_send(chk_stream_socket *s,chk_bytebuffer *b) {
+	int32_t ret = 0;
+	int32_t status = s->status;
+	do {
+		if((status & SOCKET_CLOSE) || (status & SOCKET_HCLOSE) || 
+		   (status & SOCKET_PEERCLOSE)) {
+			chk_bytebuffer_del(b);	
+			ret = -1;
+			break;
+		}
+		chk_list_pushback(&s->send_list,cast(chk_list_entry*,b));
+		if(!chk_is_write_enable(cast(chk_handle*,s))) 
+			chk_enable_write(cast(chk_handle*,s));
+	}while(0);
+	return ret;	
+}
+
+static int32_t loop_add(chk_event_loop *e,chk_handle *h,chk_event_callback cb) {
+	int32_t ret;
+	chk_stream_socket *s = cast(chk_stream_socket*,h);
+	if(!e || !h || !cb || s->status & SOCKET_CLOSE || s->status & SOCKET_HCLOSE)
+		return -1;
+	ret = chk_events_add(e,h,CHK_EVENT_READ | CHK_EVENT_WRITE | EPOLLET);
+	if(ret == 0) {
+		easy_noblock(h->fd,1);
+		s->cb = cast(chk_stream_socket_cb,cb);
+	}
+	return ret;
+}
+
+static int32_t process_write(chk_stream_socket *s) {
+	int32_t  bc,bytes,ret = 0;
+	uint32_t send_buff_size;
+	for(;;) {
+		if(0 == (bc = prepare_send(s,&send_buff_size))) return 0;
+		errno = 0;
+		if((bytes = TEMP_FAILURE_RETRY(writev(s->fd,&s->wsendbuf[0],bc))) > 0)
+			update_send_list(s,bytes);
+			if(bytes != send_buff_size) break;
+			if(chk_list_empty(&s->send_list)) {
+			 	if(s->status & SOCKET_HCLOSE) {
+					s->status |= SOCKET_CLOSE;
+					ret = -1;
+			 	}
+				break;
+			}	
+		else {
+			if(errno != EAGAIN) {
+				if(s->status & SOCKET_HCLOSE) s->status |= SOCKET_CLOSE;
+				ret = -1;
+			}
+			break;
+		}
+	}
+	return ret;
+}
+
+static void process_read(chk_stream_socket *s) {
+	int32_t  bc,bytes,unpackerr;
+	uint32_t recv_buff_size;
+	chk_decoder *decoder;
+	chk_bytebuffer *b;
+	for(;;) {
+		if(0 == (bc = prepare_recv(s,&recv_buff_size))) break;
+		errno = 0;
+		bytes = TEMP_FAILURE_RETRY(readv(s->fd,&s->wrecvbuf[0],bc));
+		if(bytes > 0) {
+			decoder = s->option.decoder;
+			decoder->update(decoder,s->next_recv_buf,s->next_recv_pos,bytes);
+			for(;;) {
+				unpackerr = 0;
+				if((b = decoder->unpack(decoder,&unpackerr))) {
+					s->cb(s,0,b);
+					chk_bytebuffer_del(b);
+					if(s->status & SOCKET_CLOSE || s->status & SOCKET_HCLOSE) 
+						return;
+				}else {
+					if(unpackerr) s->cb(s,unpackerr,NULL);
+					break;
+				}
+			};
+			if(!(s->status & SOCKET_CLOSE || s->status & SOCKET_HCLOSE))
+				update_next_recv_pos(s,bytes);
+			if(bytes != recv_buff_size) break;
+		}else {
+			if(errno != EAGAIN) {
+				s->status |= SOCKET_PEERCLOSE;
+				s->cb(s,errno,NULL);
+			}
+			return;
+		}
+	}
+}
+
+
+#else
+
+static int32_t loop_add(chk_event_loop *e,chk_handle *h,chk_event_callback cb) {
+	int32_t ret;
+	chk_stream_socket *s = cast(chk_stream_socket*,h);
+	if(!e || !h || !cb || s->status & SOCKET_CLOSE || s->status & SOCKET_HCLOSE)
+		return -1;
+	if(!chk_list_empty(&s->send_list))
+		ret = chk_events_add(e,h,CHK_EVENT_READ) || chk_events_add(e,h,CHK_EVENT_WRITE);
+	else
+		ret = chk_events_add(e,h,CHK_EVENT_READ);
+	if(ret == 0) {
+		easy_noblock(h->fd,1);
+		s->cb = cast(chk_stream_socket_cb,cb);
+	}
+	return ret;
+}
+
+static void process_write(chk_stream_socket *s) {
+	int32_t bc,bytes;
+	bc = prepare_send(s);
+	if((bytes = TEMP_FAILURE_RETRY(writev(s->fd,&s->wsendbuf[0],bc))) > 0)
+		update_send_list(s,bytes);
+		//没有数据需要发送了,停止写监听
+		if(chk_list_empty(&s->send_list)) {
+		 	if(s->status & SOCKET_HCLOSE)
+				s->status |= SOCKET_CLOSE;
+			else
+		 		chk_disable_write(cast(chk_handle*,s));
+		}	
+	else {
+		assert(errno != EAGAIN);
+		if(s->status & SOCKET_HCLOSE)
+			s->status |= SOCKET_CLOSE;
+		else
+			chk_disable_write(cast(chk_handle*,s));
+	}
+}
+
 static void process_read(chk_stream_socket *s) {
 	int32_t bc,bytes,unpackerr;
 	chk_decoder *decoder;
@@ -212,40 +433,7 @@ static void process_read(chk_stream_socket *s) {
 	}
 }
 
-
-static void release_socket(chk_stream_socket *s) {
-	chk_bytebuffer  *b;
-	chk_events_remove(cast(chk_handle*,s));	
-	if(s->next_recv_buf) chk_bytechunk_release(s->next_recv_buf);
-	if(s->option.decoder) chk_decoder_del(s->option.decoder);
-	if(s->timer) chk_timer_unregister(s->timer);
-	while((b = cast(chk_bytebuffer*,chk_list_pop(&s->send_list))))
-		chk_bytebuffer_del(b);
-	close(s->fd);
-	free(s);
-}
-
-
-static void process_write(chk_stream_socket *s) {
-	int32_t bc,bytes;
-	bc = prepare_send(s);
-	if((bytes = TEMP_FAILURE_RETRY(writev(s->fd,&s->wsendbuf[0],bc))) > 0)
-		update_send_list(s,bytes);
-		//没有数据需要发送了,停止写监听
-		if(chk_list_empty(&s->send_list)) {
-		 	if(s->status & SOCKET_HCLOSE)
-				s->status |= SOCKET_CLOSE;
-			else
-		 		chk_disable_write(cast(chk_handle*,s));
-		}	
-	else {
-		assert(errno != EAGAIN);
-		if(s->status & SOCKET_HCLOSE)
-			s->status |= SOCKET_CLOSE;
-		else
-			chk_disable_write(cast(chk_handle*,s));
-	}
-}
+#endif
 
 static void on_events(chk_handle *h,int32_t events) {
 	chk_stream_socket *s = cast(chk_stream_socket*,h);
@@ -258,7 +446,7 @@ static void on_events(chk_handle *h,int32_t events) {
 	}
 	do {
 		s->status |= SOCKET_INLOOP;
-		if(events & CHK_EVENT_READ){
+		if(events & CHK_EVENT_READ && !(s->status & SOCKET_HCLOSE)) {
 			process_read(s);	
 			if(s->status & SOCKET_CLOSE) 
 				break;								
@@ -272,22 +460,6 @@ static void on_events(chk_handle *h,int32_t events) {
 	}
 }
 
-static int32_t loop_add(chk_event_loop *e,chk_handle *h,chk_event_callback cb) {
-	int32_t ret;
-	chk_stream_socket *s = cast(chk_stream_socket*,h);
-	if(!e || !h || !cb || s->status & SOCKET_CLOSE || s->status & SOCKET_HCLOSE)
-		return -1;
-	if(!chk_list_empty(&s->send_list))
-		ret = chk_events_add(e,h,CHK_EVENT_READ) || chk_events_add(e,h,CHK_EVENT_WRITE);
-	else
-		ret = chk_events_add(e,h,CHK_EVENT_READ);
-	if(ret == 0) {
-		easy_noblock(h->fd,1);
-		s->cb = cast(chk_stream_socket_cb,cb);
-	}
-	return ret;
-}
-
 chk_stream_socket *chk_stream_socket_new(int32_t fd,chk_stream_socket_option *op) {
 	chk_stream_socket *s = calloc(1,sizeof(*s));
 	easy_close_on_exec(fd);
@@ -298,56 +470,4 @@ chk_stream_socket *chk_stream_socket_new(int32_t fd,chk_stream_socket_option *op
 	if(!s->option.decoder) 
 		s->option.decoder = cast(chk_decoder*,default_decoder_new());
 	return s;
-}
-
-int32_t last_timer_cb(uint64_t tick,void*ud) {
-	//发送已经超时,直接释放
-	chk_stream_socket *s = cast(chk_stream_socket*,ud);
-	s->timer   = NULL;
-	//timer事件在所有套接口事件之后才处理,所以这里释放是安全的
-	release_socket(s);
-	return -1;
-}
-
-void chk_stream_socket_close(chk_stream_socket *s) {
-	if((s->status & SOCKET_CLOSE) || (s->status & SOCKET_HCLOSE)) 
-		return;
-	
-	if(!(s->status & SOCKET_PEERCLOSE) && !chk_list_empty(&s->send_list) && s->loop) {
-		s->status |= SOCKET_HCLOSE;
-		chk_disable_read(cast(chk_handle*,s));
-		shutdown(s->fd,SHUT_RD);
-		//数据还没发送完,设置5秒超时等待数据发送
-		s->timer = chk_loop_addtimer(s->loop,5000,last_timer_cb,s);
-	}else {
-		s->status |= SOCKET_CLOSE;		
-		if(!(s->status & SOCKET_INLOOP)){
-			release_socket(s);
-		}
-	}
-}
-
-int32_t chk_stream_socket_send(chk_stream_socket *s,chk_bytebuffer *b) {
-	int32_t ret = 0;
-	int32_t status = s->status;
-	do {
-		if((status & SOCKET_CLOSE) || (status & SOCKET_HCLOSE) || 
-		   (status & SOCKET_PEERCLOSE)) {
-			chk_bytebuffer_del(b);	
-			ret = -1;
-			break;
-		}
-		chk_list_pushback(&s->send_list,cast(chk_list_entry*,b));
-		if(!chk_is_write_enable(cast(chk_handle*,s))) 
-			chk_enable_write(cast(chk_handle*,s));
-	}while(0);
-	return ret;	
-}
-
-void chk_stream_socket_setUd(chk_stream_socket *s,void*ud) {
-	s->ud = ud;
-}
-
-void *chk_stream_socket_getUd(chk_stream_socket *s) {
-	return s->ud;
 }
