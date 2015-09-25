f96b9a53e04f6d77db34829b256516141b7b75f0
sniperHW <sniperHW@163.com>
2015-09-25 10:55:17 +0800

    Update time.h

--- old version/src/util/time.h
+++ old version/src/util/time.h
@@ -1,46 +1,28 @@
-/*
-    Copyright (C) <2014>  <sniperHW@163.com>
+#ifndef _CHK_TIME_H
+#define _CHK_TIME_H
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-#ifndef _SYSTIME_H
-#define _SYSTIME_H
 #include <stdint.h>
 #include <time.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include <signal.h>  
 #include <pthread.h>
-#include "comm.h" 
+#include "util/chk_util.h"
 
-struct _clock
-{
+struct _clock {
     uint64_t last_tsc;
     uint64_t last_time;
 };
 
 static __thread struct _clock *__t_clock = NULL;
 
-static inline void _clock_gettime_boot(struct timespec *ts)
-{
-    if(likely(ts)) clock_gettime(CLOCK_MONOTONIC_RAW, ts);
+static inline void _clock_gettime_boot(struct timespec *ts) {
+    if(chk_likely(ts)) clock_gettime(CLOCK_MONOTONIC_RAW, ts);
 }
 
 #define NN_CLOCK_PRECISION 1000000
 
-static inline uint64_t _clock_rdtsc()
-{
+static inline uint64_t _clock_rdtsc() {
 #if (defined _MSC_VER && (defined _M_IX86 || defined _M_X64))
     return __rdtsc ();
 #elif (defined __GNUC__ && (defined __i386__ || defined __x86_64__))
@@ -62,30 +44,26 @@ static inline uint64_t _clock_rdtsc()
 #endif
 }
 
-static inline uint64_t _clock_time()
-{
+static inline uint64_t _clock_time() {
     struct timespec tv;
     _clock_gettime_boot(&tv);
     return tv.tv_sec * (uint64_t) 1000 + tv.tv_nsec / 1000000;
 }
 
-static void __clock_child_at_fork()
-{
+static void __clock_child_at_fork() {
     if(__t_clock){        
         free(__t_clock);
         __t_clock = NULL;
     }
 }
 
-static inline void _clock_init()
-{
+static inline void _clock_init() {
     __t_clock->last_tsc = _clock_rdtsc();
     __t_clock->last_time = _clock_time();
     pthread_atfork(NULL,NULL,__clock_child_at_fork);
 }
 
-static inline struct _clock *get_thread_clock()
-{
+static inline struct _clock *get_thread_clock() {
     if(!__t_clock){
         __t_clock = calloc(1,sizeof(*__t_clock));
         _clock_init();
@@ -94,8 +72,7 @@ static inline struct _clock *get_thread_clock()
 }
 
   
-static inline uint64_t systick64()
-{
+static inline uint64_t chk_systick64() {
     uint64_t tsc = _clock_rdtsc();
     if (!tsc)
         return _clock_time();
@@ -104,7 +81,7 @@ static inline uint64_t systick64()
 
     /*  If tsc haven't jumped back or run away too far, we can use the cached
         time value. */
-    if (likely(tsc - c->last_tsc <= (NN_CLOCK_PRECISION / 2) && tsc >= c->last_tsc))
+    if (chk_likely(tsc - c->last_tsc <= (NN_CLOCK_PRECISION / 2) && tsc >= c->last_tsc))
         return c->last_time;
 
     /*  It's a long time since we've last measured the time. We'll do a new
@@ -114,24 +91,10 @@ static inline uint64_t systick64()
     return c->last_time;
 }
 
-#define systick32() ((uint32_t)systick64())     
+#define chk_systick     chk_systick64
 
-#define SLEEPMS(MS) (usleep((MS)*1000))
+#define chk_systick32() ((uint32_t)chk_systick64())     
 
+#define chk_sleepms(MS) (usleep((MS)*1000))
 
-/*
-static inline char *GetCurrentTimeStr(char *buf)
-{
-	time_t _now = time(NULL);
-#ifdef _WIN
-	struct tm *_tm;
-	_tm = localtime(&_now);
-	snprintf(buf,22,"[%04d-%02d-%02d %02d:%02d:%02d]",_tm->tm_year+1900,_tm->tm_mon+1,_tm->tm_mday,_tm->tm_hour,_tm->tm_min,_tm->tm_sec);
-#else
-	struct tm _tm;
-	localtime_r(&_now, &_tm);
-	snprintf(buf,22,"[%04d-%02d-%02d %02d:%02d:%02d]",_tm.tm_year+1900,_tm.tm_mon+1,_tm.tm_mday,_tm.tm_hour,_tm.tm_min,_tm.tm_sec);
-#endif
-	return buf;
-}*/
 #endif
