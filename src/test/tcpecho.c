9b01b67a59a4039e6be7f63834554082ce3577b1
sniperHW <sniperHW@163.com>
2016-10-30 22:58:22 +0800

    none

--- src/test/tcpecho.c
+++ src/test/tcpecho.c
@@ -8,9 +8,71 @@ chk_stream_socket_option option = {
 	.decoder = NULL,
 };
 
+struct client_connection {
+	chk_stream_socket *s;
+	chk_timer         *t;
+	uint64_t           last_recv;
+	uint64_t           recv_timeout;
+};
+
+void destroy_client_connection(struct client_connection *c);
+
+int32_t timeout_cb(uint64_t tick,void *ud) {
+	struct client_connection *c = (struct client_connection*)ud;
+	if(c->last_recv + c->recv_timeout <= tick) {
+		printf("recv timeout close connection\n");
+		destroy_client_connection(c);
+		return -1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int32_t new_client_connection(struct client_connection **c,chk_stream_socket *s,uint64_t recv_timeout) {
+	if(!c || !s) {
+		return -1;
+	}
+
+	*c = calloc(1,sizeof(**c));
+
+	if(!c) {
+		return -1;
+	}
+
+	(*c)->s = s;
+
+	if(recv_timeout > 0) {
+		(*c)->recv_timeout = recv_timeout;
+		(*c)->last_recv = chk_systick64();
+		(*c)->t = chk_loop_addtimer(loop,100,timeout_cb,(void*)*c);
+		if(!(*c)->t) {
+			free(*c);
+			(*c) = NULL;
+			return -1;
+		}
+	}
+	return 0;
+}
+
+void destroy_client_connection(struct client_connection *c) {
+	if(c) {
+		chk_timer_unregister(c->t);
+		if(c->s) {
+			chk_stream_socket_close(c->s,5000);
+			c->s = NULL;
+		}
+		free(c);
+	}
+}
+
 
 void data_event_cb(chk_stream_socket *s,chk_bytebuffer *data,int32_t error) {
+	
+	struct client_connection *c = chk_stream_socket_getUd(s);
+
 	if(data){
+		c->last_recv = chk_systick64();
 		if(0!= chk_bytebuffer_append(data,(uint8_t*)"hello",strlen("hello"))){
 			printf("data is readonly\n");
 		}
@@ -18,7 +80,7 @@ void data_event_cb(chk_stream_socket *s,chk_bytebuffer *data,int32_t error) {
 	}
 	else{
 		printf("socket close:%d\n",error);
-		chk_stream_socket_close(s,5000);
+		destroy_client_connection(c);
 	}
 	
 }
@@ -26,7 +88,14 @@ void data_event_cb(chk_stream_socket *s,chk_bytebuffer *data,int32_t error) {
 void accept_cb(chk_acceptor *a,int32_t fd,chk_sockaddr *addr,void *ud,int32_t err) {
 	printf("accept_cb\n");
 	chk_stream_socket *s = chk_stream_socket_new(fd,&option);
-	chk_loop_add_handle(loop,(chk_handle*)s,data_event_cb);
+	struct client_connection *c;
+	if(0 != new_client_connection(&c,s,5000)) {
+		chk_stream_socket_close(s,0);
+	}
+	else {
+		chk_stream_socket_setUd(s,c);
+		chk_loop_add_handle(loop,(chk_handle*)s,data_event_cb);
+	}
 }
 
 static void signal_int(void *ud) {
