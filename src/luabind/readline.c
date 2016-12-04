7ca286358080e9d5a33a422ba21300b76bc91ca3
sniperHW <sniperHW@163.com>
2016-12-04 10:06:15 +0800

    添加completion

--- src/luabind/readline.c
+++ src/luabind/readline.c
@@ -1,6 +1,30 @@
 #include "lua/chk_lua.h"
 
 #include "linenoise.h"
+
+void completion(const char *buf, linenoiseCompletions *lc) {
+    
+    switch(buf[0]){
+    	case 'r':{
+        	linenoiseAddCompletion(lc,"redis_execute");
+        	linenoiseAddCompletion(lc,"result");
+    	}
+    	break;
+    	case 'e':{
+    		linenoiseAddCompletion(lc,"exit");  
+    	}
+    	break;
+    	case 'p':{
+      		linenoiseAddCompletion(lc,"print");   		
+    	}
+    	break;
+    	default:{
+    		return;
+    	}
+
+    }
+}
+
 int32_t chk_lua_readline(lua_State *L)
 {
 	const char *prompt = lua_tostring(L,1);
@@ -31,6 +55,7 @@ int32_t luaopen_readline(lua_State *L)
 	//lua_newtable(L);
 	//SET_FUNCTION(L,"Readline",chk_lua_readline);
 	//linenoiseHistoryLoad("history.txt");
+    linenoiseSetCompletionCallback(completion);	
 	lua_pushcfunction(L,chk_lua_readline);
 	return 1;
 }
