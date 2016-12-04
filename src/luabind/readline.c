311171830412bb252aea9b12d9c10e23048b5163
sniperHW <sniperHW@163.com>
2016-12-04 09:36:03 +0800

    使用linenoise替代readline

--- src/luabind/readline.c
+++ src/luabind/readline.c
@@ -1,5 +1,5 @@
 #include "lua/chk_lua.h"
-#include "readline/readline.h"
+/*#include "readline/readline.h"
 #include "readline/history.h"
 
 int32_t chk_lua_readline(lua_State *L)
@@ -24,12 +24,39 @@ int32_t chk_lua_readline(lua_State *L)
 		return 0;
 	}
 
+}*/
+
+#include "linenoise.h"
+int32_t chk_lua_readline(lua_State *L)
+{
+	const char *prompt = lua_tostring(L,1);
+	if(!prompt) {
+		return 0;
+	}
+
+	char *line = linenoise(prompt);
+
+	if(NULL != line) {
+
+		if(*line) {
+			linenoiseHistoryAdd(line);
+		}
+		lua_pushstring(L,line);
+		free(line);
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
+
+
 
 int32_t luaopen_readline(lua_State *L)
 {
 	//lua_newtable(L);
 	//SET_FUNCTION(L,"Readline",chk_lua_readline);
+	//linenoiseHistoryLoad("history.txt");
 	lua_pushcfunction(L,chk_lua_readline);
 	return 1;
 }
