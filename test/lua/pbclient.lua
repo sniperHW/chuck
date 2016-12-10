71060c6220a004c440a8bac3cf8788650b0f8175
sniperHW <sniperHW@163.com>
2016-12-10 16:47:08 +0800

    add pb test

---
+++ test/lua/pbclient.lua
@@ -0,0 +1,61 @@
+package.path = './lib/?.lua;'
+package.cpath = './lib/?.so;'
+
+local protobuf = require "protobuf"
+local chuck = require("chuck")
+local socket = chuck.socket
+local event_loop = chuck.event_loop.New()
+local log = chuck.log
+
+local addr = io.open("test/lua/addressbook.pb","rb")
+local pb_buffer = addr:read "*a"
+addr:close()
+
+protobuf.register(pb_buffer)
+
+local event_loop = chuck.event_loop.New()
+
+socket.stream.ip4.dail(event_loop,"127.0.0.1",8010,function (fd,errCode)
+	if 0 ~= errCode then
+		print("connect error:" .. errCode)
+		return
+	end
+	local conn = socket.stream.New(fd,4096)
+	if conn then
+
+		print("connect ok")
+
+		local addressbook = {
+			name = "Alice",
+			id = 12345,
+			phone = {
+				{ number = "1301234567" },
+				{ number = "87654321", type = "WORK" },
+			}
+		}
+
+		local buff = chuck.buffer.New()
+		local code = protobuf.encode("tutorial.Person", addressbook)
+		buff:AppendStr(code)
+		conn:Send(buff)
+
+		conn:Start(event_loop,function (data)
+			if data then 
+				print("got response")
+				conn:Close()
+			else
+				print("client disconnected") 
+				conn:Close()
+			end
+			event_loop:Stop()
+		end)
+
+	end
+end)
+
+event_loop:WatchSignal(chuck.signal.SIGINT,function()
+	print("recv SIGINT stop client")
+	event_loop:Stop()
+end)
+
+event_loop:Run()
---
+++ test/lua/pbserver.lua
@@ -0,0 +1,45 @@
+package.path = './lib/?.lua;'
+package.cpath = './lib/?.so;'
+
+local protobuf = require "protobuf"
+local chuck = require("chuck")
+local socket = chuck.socket
+local event_loop = chuck.event_loop.New()
+local log = chuck.log
+
+local addr = io.open("test/lua/addressbook.pb","rb")
+local buffer = addr:read "*a"
+addr:close()
+
+protobuf.register(buffer)
+
+
+local server = socket.stream.ip4.listen(event_loop,"127.0.0.1",8010,function (fd)
+	local conn = socket.stream.New(fd,4096)
+	if conn then
+		conn:Start(event_loop,function (data)
+			if data then
+				local decode = protobuf.decode("tutorial.Person" , data:Content())
+				print("--------------------Person------------------------")
+				print("name:" .. decode.name .. " id:" .. decode.id)
+				for k,v in pairs(decode.phone) do
+					print("number:" .. v.number .. " " .. v.type or "")
+				end
+				conn:Close()
+			else
+				log.SysLog(log.info,"client disconnected") 
+				conn:Close() 
+			end
+		end)
+	end
+end)
+
+if server then
+	log.SysLog(log.info,"server start")	
+
+	event_loop:WatchSignal(chuck.signal.SIGINT,function()
+		log.SysLog(log.info,"recv SIGINT stop server")
+		event_loop:Stop()
+	end)	
+	event_loop:Run()
+end
