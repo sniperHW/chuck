1e98a8ebd4fcda37e62b21082683b0c7c211c65e
sniperHW <sniperHW@163.com>
2017-05-09 22:05:33 +0800

    添加redis promise接口

--- example/promise_redis.lua
+++ example/promise_redis.lua
@@ -2,53 +2,20 @@ package.cpath = './lib/?.so;'
 package.path = './lib/?.lua;'
 
 local chuck = require("chuck")
---local redis = require("redis")
-local redis = chuck.redis
-local promise = require("promise")
+local redis = require("redis")
 local event_loop = chuck.event_loop.New()
-
-local function Connect(ip,port)
-   return promise.new(function(resolve,reject)
-      local err = redis.Connect_ip4(event_loop,ip,port,function (conn,err)
-            if err then
-               reject(err)
-            else
-               resolve(conn)
-            end
-         end)
-      if err then
-         reject(err)
-      end
-   end)
-end
-
-local function Execute(conn,cmd,...)
-   local args = {...}
-   return promise.new(function (resolve,reject)
-      local err = conn:Execute(function (reply,err)
-            if err then
-               reject(err)
-            else
-               resolve(reply)
-            end
-         end,cmd,table.unpack(args))
-      if err then
-         reject(err)
-      end
-   end)
-end
 
 local redis_conn
 
-Connect("127.0.0.1",8010):andThen(function (conn)
+redis.ConnectPromise(event_loop,"127.0.0.1",6379):andThen(function (conn)
    print("connect to redis ok")
    redis_conn = conn
-   return Execute(redis_conn,"set","sniper","hw")
+   return redis.ExecutePromise(redis_conn,"set","sniper","hw")
 end):andThen(function (conn)
-   return Execute(redis_conn,"get","sniper")
+   return redis.ExecutePromise(redis_conn,"get","sniper")
 end):andThen(function (reply)
    print(reply)
-   return Execute(redis_conn,"ping")
+   return redis.ExecutePromise(redis_conn,"ping")
 end):andThen(function (reply)
    print(reply)
 end):catch(function (err)
--- lib/redis.lua
+++ lib/redis.lua
@@ -1,4 +1,5 @@
 local chuck = require("chuck")
+local promise = require("Promise")
 local redis = chuck.redis
 
 M = {}
@@ -27,5 +28,38 @@ function M.Connect(eventLoop,ip,port,callback)
 	return redis.Connect_ip4(eventLoop,ip,port,callback)
 end
 
+function M.ConnectPromise(eventLoop,ip,port)
+   return promise.new(function(resolve,reject)
+      local err = redis.Connect_ip4(eventLoop,ip,port,function (conn,err)
+            if err then
+               reject(err)
+            else
+               resolve(conn)
+            end
+         end)
+      if err then
+         reject(err)
+      end
+   end)	
+end
+
+function M.ExecutePromise(conn,cmd,...)
+	local args = {...}
+   	return promise.new(function (resolve,reject)
+   		if nil == conn then
+   			return reject("connection closed")
+   		end
+      	local err = conn:Execute(function (reply,err)
+            if err then
+               reject(err)
+            else
+               resolve(reply)
+            end
+         end,cmd,table.unpack(args))
+      	if err then
+         	reject(err)
+      	end
+   	end)	
+end
 
 return M
