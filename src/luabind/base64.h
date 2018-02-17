77e35d321e98a28bb116a117d0c8a6e44e7adfbe
sniperHW <sniperHW@163.com>
2018-02-17 20:49:11 +0800

    none

--- src/luabind/base64.h
+++ src/luabind/base64.h
@@ -4,8 +4,8 @@ static int lua_encode(lua_State *L){
 	size_t len;
 	const unsigned char *input = (const unsigned char*)lua_tolstring(L,-1,&len);
 	const char *output = b64_encode(input,len);
-	lua_pushstring(L,b64_encode(input,len));
-	free(output);	
+	lua_pushlstring(L,output,strlen(output));
+	free((void*)output);	
 	return 1;	
 }
 
@@ -14,7 +14,7 @@ static int lua_decode(lua_State *L){
 	const char *input = (const char*)lua_tolstring(L,-1,&len1);
 	const char *output = (const char*)b64_decode_ex(input,len1,&len2);
 	lua_pushlstring(L,output,len2);
-	free(output);
+	free((void*)output);
 	return 1;	
 }
 
