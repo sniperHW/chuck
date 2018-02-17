e7dbc2e9b8f65c0547dc1a0cc9b41d95c6d4e8ad
sniperHW <sniperHW@163.com>
2018-02-17 18:26:56 +0800

    none

--- src/util/base64.c
+++ src/util/base64.c
@@ -24,7 +24,8 @@ b64_encode (const unsigned char *src, size_t len) {
   if (NULL == enc) { return NULL; }
 
   // parse until end of source
-  while (len-- > 0) {
+  //while (len-- > 0) {
+  for(; len > 0; --len){
     // read up to 3 bytes at a time into `tmp'
     tmp[i++] = *(src++);
 
