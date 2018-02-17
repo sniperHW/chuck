d7782950ded0bf2a098850b71edbc866f5830ec0
sniperHW <sniperHW@163.com>
2018-02-17 21:56:24 +0800

    none

--- src/luabind/base64.h
+++ src/luabind/base64.h
@@ -1,5 +1,6 @@
 #include "util/base64.h"
 
+/*
 static int lua_encode(lua_State *L){
 	size_t len;
 	const unsigned char *input = (const unsigned char*)lua_tolstring(L,-1,&len);
@@ -17,9 +18,85 @@ static int lua_decode(lua_State *L){
 	free((void*)output);
 	return 1;	
 }
+*/
+int lua_f_base64_encode(lua_State *L)
+{
+    const unsigned char *src = NULL;
+    size_t slen = 0;
+
+    if(lua_isnil(L, 1)) {
+        src = (const unsigned char *) "";
+
+    } else {
+        src = (const unsigned char *) luaL_checklstring(L, 1, &slen);
+    }
+
+    unsigned char *end = malloc(base64_encoded_length(slen));
+    int nlen = base64_encode(end, src, slen);
+    lua_pushlstring(L, (char *) end, nlen);
+    free(end);
+    return 1;
+}
+
+int lua_f_base64_decode(lua_State *L)
+{
+    const unsigned char *src = NULL;
+    size_t slen = 0;
+
+    if(lua_isnil(L, 1)) {
+        src = (const unsigned char *) "";
+
+    } else {
+        src = (unsigned char *) luaL_checklstring(L, 1, &slen);
+    }
+
+    unsigned char *end = malloc(base64_decoded_length(slen));
+    int nlen = base64_decode(end, src, slen);
+    lua_pushlstring(L, (char *) end, nlen);
+    free(end);
+    return 1;
+}
+
+int lua_f_base64_encode_url(lua_State *L)
+{
+    const unsigned char *src = NULL;
+    size_t slen = 0;
+
+    if(lua_isnil(L, 1)) {
+        src = (const unsigned char *) "";
+
+    } else {
+        src = (const unsigned char *) luaL_checklstring(L, 1, &slen);
+    }
+
+    unsigned char *end = malloc(base64_encoded_length(slen));
+    int nlen = base64_encode_url(end, src, slen);
+    lua_pushlstring(L, (char *) end, nlen);
+    free(end);
+    return 1;
+}
+
+int lua_f_base64_decode_url(lua_State *L)
+{
+    const unsigned char *src = NULL;
+    size_t slen = 0;
+
+    if(lua_isnil(L, 1)) {
+        src = (const unsigned char *) "";
+
+    } else {
+        src = (unsigned char *) luaL_checklstring(L, 1, &slen);
+    }
+
+    unsigned char *end = malloc(base64_decoded_length(slen));
+    int nlen = base64_decode_url(end, src, slen);
+    lua_pushlstring(L, (char *) end, nlen);
+    free(end);
+    return 1;
+}
 
 static void register_base64(lua_State *L) {
 	lua_newtable(L);
-	SET_FUNCTION(L,"encode",lua_encode);
-	SET_FUNCTION(L,"decode",lua_decode);
+	SET_FUNCTION(L,"encode",lua_f_base64_encode);
+	SET_FUNCTION(L,"decode",lua_f_base64_decode);
 }
--- src/util/base64.h
+++ src/util/base64.h
@@ -1,50 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <sys/types.h>
 
-/**
- * `b64.h' - b64
- *
- * copyright (c) 2014 joseph werle
- */
+#ifndef _BASE64_H
+#define _BASE64_H
 
-#ifndef B64_H
-#define B64_H 1
-
-/**
- * Base64 index table.
- */
-
-static const char b64_table[] = {
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-  'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-  'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-  'w', 'x', 'y', 'z', '0', '1', '2', '3',
-  '4', '5', '6', '7', '8', '9', '+', '/'
-};
-
-/**
- * Encode `unsigned char *' source with `size_t' size.
- * Returns a `char *' base64 encoded string.
- */
-
-char *
-b64_encode (const unsigned char *, size_t);
-
-/**
- * Dencode `char *' source with `size_t' size.
- * Returns a `unsigned char *' base64 decoded string.
- */
-unsigned char *
-b64_decode (const char *, size_t);
-
-/**
- * Dencode `char *' source with `size_t' size.
- * Returns a `unsigned char *' base64 decoded string + size of decoded string.
- */
-unsigned char *
-b64_decode_ex (const char *, size_t, size_t *);
-
-
+#define base64_encoded_length(len) (((len + 2) / 3) * 4)
+#define base64_decoded_length(len) (((len + 3) / 4) * 3)
+int base64_encode(unsigned char *dst, const unsigned char *src, int len);
+int base64_encode_url(unsigned char *dst, const unsigned char *src, int len);
+int base64_decode(unsigned char *dst, const unsigned char *src, size_t slen);
+int base64_decode_url(unsigned char *dst, const unsigned char *src, size_t slen);
 
 #endif
